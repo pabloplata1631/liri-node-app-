259571c60dbc3ade0cb9143e530e74a8e3a29618
Pablo Plata <pplata1631@gmail.com>
2018-04-15 23:44:02 -0500

    first changes

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('This is Loaded')
+exports.twitterKeys = {
+    consumer_key:	'CkNP5cV3vE2pqKkRzJoHsmXkw',
+    consumer_secret: 	'2YaUA8kOe8uXT4IpkrghL39ZmtpDSQFC4w03t8j0uh9X6T1iYr',
+    access_tokent_key: 	'711890972-F8TbYCNiuVqQixQHtUOtBmudmAdsvHmHzjUb56wV',
+    access_token_secret: 	'uDIdWWLuOw4p5EakJJkydfPkQqAzobJ5or2iJVjwiTLhS',
+
+}
